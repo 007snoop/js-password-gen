967f3d2a23c578344fbddff1b3fe1e1a250ff8a0
Colin Yetman <colinguitarist@gmail.com>
2025-01-17 12:26:46 -0330

    1.1.1 update, added upper case to pool. Moved random into for loop for clearer code

--- password-gen/index.js
+++ password-gen/index.js
@@ -9,24 +9,28 @@ const process = require("process");
 const arguments = process.argv.slice(2);
 
 // set up character pool for password
-const characterPool = "abcdefghijklmnopqrstuvwyxz"
+
+// version 1.1.1 update: Add upper case to character pool
+const characterPool = "abcdefghijklmnopqrstuvwyxzABCDEFGHIJKLMNOPQRSTUVWYXZ"
 // set up default length if nothing is set
 const passwordDefaultLength = 8;
 
 
-// random section using the Math.random module
-const randomIndex = Math.floor(Math.random() * characterPool.length);
-const randomCharacter = characterPool[randomIndex];
 
-let password = ""; // make the password a string, this will be changed later
+// moved random values into for loop for cleaner code.
+
+
+
 
 
 // use a loop to go over your pool
 
+let password = ""; // make the password a string, this will be changed later
 for (let i = 0; i < passwordDefaultLength; i++) {
-
     const randomIndex = Math.floor(Math.random() * characterPool.length);
-    password += characterPool[randomIndex];
+    const randomCharacter = characterPool[randomIndex];
+    
+    password += randomCharacter;
 }
 
 
