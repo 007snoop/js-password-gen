ae1e6ed7d6a320a531ca708873ba575348667177
Colin Yetman <colinguitarist@gmail.com>
2025-01-20 17:31:01 -0330

    added 1.5.3 updates to code revisions.

--- password-gen/index.js
+++ password-gen/index.js
@@ -53,21 +53,23 @@ if (lengthIndex !== -1 && arguments[lengthIndex + 1]) {
 }
 // moved random values into for loop for cleaner code.
 // version 1.3.0 update: removed upper case from pool, add "--caps" flag for upper case
-if (arguments.includes("--caps")) {
+// version 1.5.3: added other keywords to be captured by app
+// changed to create an array of valid flags for this function and then checks if any of the flags are in the user input
+if (["--c", "--caps", "--capital", "--capitals"].some(flag => arguments.includes(flag))) {
     let characterPoolUpper = "ABCDEFGHIJKLMNOPQRSTUVWYXZ";
     characterPool += characterPoolUpper;
     console.log("Upper Case Incldued for Password Generation.");
 }
 
 // version 1.5.0: add integer values to character pool as my dumbass forgot them before
-if (arguments.includes("--number")) {
+if (["--n", "--number", "--numbers", "--num", "--numb"].some(flag => arguments.includes(flag))) {
     let numberPool = "0123456789"
     characterPool += numberPool;
     console.log("Numbers Included for Password Generation");
 }
 
 // version 1.4.0 update: add special flag capture and append special to pool for generation
-if (arguments.includes("--special")) {
+if (["--s", "--spec", "--special", "--specials"].some(flag => arguments.includes(flag))) {
     let characterPoolSpecial = "!@#$%^&*()_+[]{}|;:,.<>?";
     characterPool += characterPoolSpecial;
     console.log("Special Characters Included for Password Generation.");
